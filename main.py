a914ec437bb310810671ff59873faf8c8a81e02a
qwertyrgb <68125220+qwertyrgb@users.noreply.github.com>
2022-10-21 08:27:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 ## ! WELCOME TO TROLLAND !
 ## 🄯 Copyleft 2022 - All wrongs reversed to Lamereary Industries - ширАко and SoulTaker
-print('penis')
+
+try: #import basique de sauvegarde
+  from saves import *
+  Weapon=eval(Weapon)
+except ModuleNotFoundError: #si non sauvegardé, pas de fichier de sauvegarde
+  Weapon=''
+
+class Weapons: #définit toutes les armes du jeu
+  def __init__(self,tier,name,description,bonus=''): #Pas forcément de bonus
+    self.tier=tier
+    self.name=name
+    self.description=description
+    
+  def info(self):# ex: Rare tier: Magical Tome {MP +5, ATQ +10} \n\t this weapon does emotional damage
+    print(self.tier,' tier: ',self.name,', '.join(tuple(': +'.join((i,str(a[i]))) for i in a)), description) #flemme de commenter + peut être optimisé
+
+ Magical_Tome=Weapons('Rare','Magical Tome','this weapon does emotional damage',{'MP':5, 'ATQ':10}) #voir power-system.txt
