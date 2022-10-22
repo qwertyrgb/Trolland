38b41c1044bcae02c95c7fe9095d6f8a3195622b
qwertyrgb <68125220+qwertyrgb@users.noreply.github.com>
2022-10-22 10:42:31 +0200

    Update dialogue_fr.py

--- dialogue_fr.py
+++ dialogue_fr.py
@@ -1,1 +1,1 @@
-welcome="Bienvenue à Trolland, ceci est un jeu de haute fantaisie uniquement sur console avec un bon scénario et pas mal de combats."
+Welcome="Bienvenue à Trolland, ceci est un jeu de haute fantaisie uniquement sur console avec un bon scénario et pas mal de combats."
