b4bfb62623c47bc11755846456663b0a2141a8e0
qwertyrgb <68125220+qwertyrgb@users.noreply.github.com>
2022-10-25 10:47:54 +0200

    Update dialogue_fr.py

--- dialogue_fr.py
+++ dialogue_fr.py
@@ -1,1 +1,18 @@
+# Fichier de traduction, différent pour dialogue_en.py et dialogue_es.py
+
+# Tiers des armes
+Common_Tier='Commun'
+Rare_Tier='Rare'
+Hero_Tier='Heroïque'
+Relic_Tier='Relique'
+Arcanic_Tier='Arcanique'
+
+# Les armes
+Wooden_Shield_Name='Bouclier en bois'
+Wooden_Shield_Description=''
+Wooden_Sword_Name='Épée en bois'
+Wooden_Sword_Description=''
+Old_Grimoire_Name='Vieux grimoire'
+Old_Grimoire_Description=''
+
 Welcome="Bienvenue à Trolland, ceci est un jeu de haute fantaisie uniquement sur console avec un bon scénario et pas mal de combats."
