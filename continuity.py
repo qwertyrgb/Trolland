4c61f62c9a2d50730eb05b15bd7f6946b4c6f8cd
SoulTaker <76255525+SoulTaker-exe@users.noreply.github.com>
2022-11-05 23:16:17 +0100

    Update continuity.py

--- continuity.py
+++ continuity.py
@@ -1,18 +1,19 @@
 #Next variable : 
-#currentAction="4" (global index of Desc1_4_books="You see three heavy books")
-#Next[currentAction]=AskP1_5_books (variable the program should print next)
+#currentAction=4 (global index of Desc1_4_books="You see three heavy books")
+#Next[currentAction]=AskP1_books (variable the program should print next)
 
 Next=['Desc1_1_room', #boot
       'AskP1_room', #1
       'Desc1_3_note', #2
       'AskP1_downstairs', #3
-      'AskP1_book', #4
-      'AskP1_book', #5
-      'AskP1_book', #6
-      'AskP1_book', #7
+      'AskP1_books', #4
+      'AskP1_books', #5
+      'AskP1_books', #6
+      'AskP1_books', #7
       'Desc1_9_boom', #8
       'AskP1_boom', #9
       'AskP1_room', #10
       'AskP1_downstairs,' #11
       'AskP1_backOutside', #12
      ]
+
