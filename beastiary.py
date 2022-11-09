832656b8eaab7874d7d89e318e64e84cf1a46319
SoulTaker <76255525+SoulTaker-exe@users.noreply.github.com>
2022-11-09 18:46:44 +0100

    Create beastiary.py
    troll

---
+++ beastiary.py
@@ -0,0 +1,10 @@
+InjuredTroll_AL=["Smash"]
+
+InjuredTroll=["DEF=0",
+              "ATQ=900",
+              "HP=6",
+              "MAX_HP=1750",
+              "STA=150",
+              "MP=150",
+              "WEAP=Sword of Justice",
+              InjuredTroll_AL]
