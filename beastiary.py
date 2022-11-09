70782c8b52012ce8a484b20d6e52e39a94267557
SoulTaker <76255525+SoulTaker-exe@users.noreply.github.com>
2022-11-09 19:03:02 +0100

    Update beastiary.py
    beastiary setup + injured troll updates

--- beastiary.py
+++ beastiary.py
@@ -1,9 +1,25 @@
+#1 - MONSTER ATTACK LIST
+
+#attack - attack stats - [SCA, Attack special effect]
+
+Smash=["20%", "none"]
+
+#2 - MONSTER WEAPON LIST 
+
+#weapon_EL - weapon Effect List - [Stat bonus (1:4, can be undefined)]
+#weapon - [[EL], special effect]
+
+Sword_of_justice_EL=["ATQ=+100"]
+Sword_of_justice=[Sword_of_justice_EL, "All attacks deal 30% of the target's MAX_HP"]
+
+#3 - BEASTS
+
 #beast_AL - beast Attack List - [Attacks (1:6, can be undefined) of Beast]
 #beast - beast stats - [DEF, ATQ, HP, MAX_HP, STA, MP, WEAP, [AL]]
 
 InjuredTroll_AL=["Smash"]
 InjuredTroll=["DEF=0",
-              "ATQ=900",
+              "ATQ=800",
               "HP=6",
               "MAX_HP=1750",
               "STA=150",
