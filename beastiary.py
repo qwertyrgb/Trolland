bf00c58ad9212675c571cbcb05a7e8f6170705d0
SoulTaker <76255525+SoulTaker-exe@users.noreply.github.com>
2022-11-09 18:50:52 +0100

    Update beastiary.py

--- beastiary.py
+++ beastiary.py
@@ -1,5 +1,7 @@
-InjuredTroll_AL=["Smash"]
+#beast_AL - beast Attack List - [Attacks (1:6, can be undefined) of Beast]
+#beast - beast stats - [DEF, ATQ, HP, MAX_HP, STA, MP, WEAP, [AL]]
 
+InjuredTroll_AL=["Smash"]
 InjuredTroll=["DEF=0",
               "ATQ=900",
               "HP=6",
