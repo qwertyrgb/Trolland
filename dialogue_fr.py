9fcda1284973a35b1d2dab3a54e5c32e38323695
qwertyrgb <68125220+qwertyrgb@users.noreply.github.com>
2022-11-13 13:52:55 +0100

    Update dialogue_fr.py

--- dialogue_fr.py
+++ dialogue_fr.py
@@ -7,13 +7,14 @@ Hero_Tier='Heroïque'
 Relic_Tier='Relique'
 Arcanic_Tier='Arcanique'
 Easter_Egg_Tier='Easter Egg'
+Monster_Tier='Monstre'
 
 # Les armes
 Wooden_Shield_Name='Bouclier en bois'
 Wooden_Shield_Description="Un bouclier en bois qui n'a pas l'air très résistant."
 Wooden_Sword_Name='Épée en bois'
 Wooden_Sword_Description='Une épée en bois dont la forme est imparfaite et qui semble difficile à utiliser.'
-Old_Grimoire_Name='Vieux grimoire'
+Old_Grimoire_Name='Grimoire ancien'
 Old_Grimoire_Description='Un grimoire avec des caractères illisibles et des illustrations étranges. Vous sentez une légère aura magique en émaner.'
 Priest_Shield_Name='Bouclier du Prêtre'
 Priest_Shield_Description="Un bouclier avec une crois dessus et l'emblème de l'église, il semble solide."
@@ -26,10 +27,12 @@ Faith_Shield_Description="Un bouclier lourd fait de bois et d'un minéral inconn
 Faith_shield_fun_desc="La défense a une chance de 10% d'être doublée"
 Michael_Sword_Name='Épée de Michael'
 Michael_Sword_Description="Une belle épée, faite avec un solide métal blanc et décorée avec de l'or. Il est dit qu'elle a jadis appartenu à un archange."
+Michael_sword_fun_desc="L'Attaque a une chance de 10% d'être doublée"
 Dantalion_Anti_Bible_Name='Anti-Bible de Dantalion'
 Dantalion_Anti_Bible_Description="Un livre rempli de caractères anciens illisibles écrits avec du sang. Il est dit qu'un puissant démon l'a écrit, en échange des vies de 7 humains."
 Troll_King_Shield_Name='Bouclier du Roi Troll'
 Troll_King_Shield_Description='Un bouclier, ressemblant plus à un morceau de Trollium condensé de mana rouge, apparemment indestructible, qui était utilisé par le précédent Roi Troll.'
+Trollking_fun_desc="Les dégats ont une chance de 25% d'être ignorés"
 Conquerer_Sword_Name='Épée du Conquérant'
 Conquerer_Sword_Description='Une magnifique épée blanche, confectionnée avec le métal sacré pour une épée sans doute parfaite. Elle est fabuleusement engravée et décorée avec des gemmes et du métal rouge parfaitement équilibrés.'
 Conquerer_Sword_fun_desc="Les dégats sont doublés s'il n'y a qu'un seul enemi"
@@ -38,37 +41,82 @@ Elf_King_Epitome_Description="Un manuscrit contenant tout le savoir magique du R
 Elfking_Epitome_fun_desc="S'il y a plusieurs enemis, ils ont tous une chance de 25% d'être étourdis"
 Purgatory_Door_Name='Porte du Purgatoire'
 Purgatory_Door_Description="Une relique divine extrêmement puissante. Il est dit que ce bouclier fut confectionné par des forces supéreures quand elles ont crée le Purgatoire pour garder les âmes vacantes à l'intérieur, mais il est aussi dit qu'elle fut détruite par Satan pendant l'Affrontement d'en Haut."
-Purgatory_Door_fun_name="Retourne 20% des dégats à l'attaquant"
+Purgatory_Door_fun_desc="Retourne 20% des dégats à l'attaquant"
 Divine_Light_Blade_Name='Lame Divine de Lumière'
 Divine_Light_Blade_Description="Une relique divine extrêmement puissante. Les légendes disent que cette épée a été créée à partir de l'annulaire gauche de Dieu, elle peut couper à travers n'importe quel matériau non divin."
-Divine_Light_Blade_fun="Les dégats sont doublés si l'attaquant a plus de 50% PV restants"
+Divine_Light_Blade_fun_desc="Les dégats sont doublés si l'attaquant a plus de 50% PV restants"
 Satan_Profecy_Name='Profécie de Satan'
 Satan_Profecy_Description='Une relique divine extrêmement puissante. Elle contient la moitié des connaissances de Satan en elle, et une éternité de savoir noir et de magie.'
-Satan_Profecy_fun="Inflige une malédiction sur l'enemi qui draine 20% de leur PV_MAX par tour"
-Hugo_Wives_Name='Femmes à Hugo'
-Hugo_Wives_Description='AH LA POLYGAMIE'
-Paul_Cum_Shield_Name='Bouclier de Cum de Paul'
-Paul_Cum_Shield_Description='Résultats du premier décembre'
-Paul_Cum_Shield_fun="Tous les dégats reçus sont renvoyés à l'attaquant et doublés"
-Mael_Schlong_Name='Schlong de Maël'
-Mael_Schlong_Description="La chose préférée d'Elise"
-Mael_Schlong_fun='Attaque tous les ennemis sur le champ de bataille'
-Arty_Pride_Book_Name='Arty il est gay'
-Arty_Pride_Book_Description='Pas de homo'
-Arty_Pride_Book_fun="Chaque attaque étourdit tous le ennemis"
+Satan_Profecy_fun_desc="Inflige une malédiction sur l'enemi qui draine 20% de leur PV_MAX par tour"
+Justice_Sword_Name='Sword of Justice'
+Justice_Sword_Description='The sword of the Troll'
 
 Welcome='Bienvenue à Trolland, ceci est un jeu de haute fantaisie uniquement sur console avec un bon scénario et pas mal de combats.'
 
-default_ask="Que choisissez-vous de faire?"
-
+Default_ask="Que choisissez-vous de faire?"
+Not_Accepted='Réponse invalide'
 #ARC 1 : Village Tutoriel 
 
-desc1_9_magicScience=""
-desc1_8_history=""
-desc1_7_holyGuide=""
-qOut1_6_books=[desc1_7_holyGuide, desc1_8_history, desc1_9_magicScience]
-askP1_5_books=['Lire "Le Guide Sacré"', 'Lire "Histoire de Trolland"', 'Lire "La Science de la Magie"']
-desc1_4_books="You see three heavy books"
-qOut1_3_room=[a, desc1_4_books]
-askP1_2_room=[default_ask, "Descendre par les excaliers", "Regarder"]
-desc1_1_room="Vous vous levez dans une pièce sombre, vous allumez une bougie. Vous êtes chez vous dans votre chambre, il y a des vieux libres dans la bibliothèque à votre gauche, et un escalier descendant à votre droite."
+Desc1_1_room="Vous vous levez dans une pièce sombre, vous allumez une bougie. Vous êtes chez vous dans votre chambre, il y a des vieux libres dans la bibliothèque à votre gauche, et un escalier descendant à votre droite."
+AskP1_room=[Default_ask, "Descendre par les escaliers", "Regarder les livres"]
+
+Desc1_4_books='Vous voyez trois gros livres'
+AskP1_books=[Default_ask,'Lire "Le Guide Sacré"', 'Lire "Histoire de Trolland"', 'Lire "La Science de la Magie"','Sortir']
+Desc1_5_holyGuide='Tuez le the Troll.'
+Desc1_6_history='Un jour, Sylvain Durif détruira a Terre.'
+Desc1_7_magicScience='Pour les trucs magiques, achetez du mana.'
+
+QOut1_books=["Desc1_5_holyGuide", "Desc1_6_history", "Desc1_7_magicScience", "AskP1_room"]
+
+Desc1_2_downstairs="Vous descendez les vieux escaliers grinçants en bois alors qu'un salon chaleureux et accueillant se révèle à vos yeux. C'est petit, mais plein de bienveillance. Vous remarquez que votre mère n'est pas là. Vous voyez une note sur la table, a coté d'un pot frais de lys couleur sang."
+Desc1_3_note="La note dit : \"Enfin reveillé :D ? Je vais acheter des charges de mana en ville, je rentre bientôt. Je t'aime, Maman <3\""
+AskP1_downstairs=[Default_ask, 'Aller dehors', 'Remonter les escaliers', 'Regarder autour de vous']
+Desc1_10_backUpstairs="Vous remontez les escaliers, et arrivez dans votre chambre."
+Desc1_11_lookAround="À votre droite vous observez un âtre en pierre, avec, au-dessus, une cheminée en brique, sans aucun feu à l'intérieur. Devant elle se trouve un canapé en cuir, lui faisant face avec une table basse. La maison est décorée de croquis d'arbres accrochés au mur, l'un d'eux attire votre attention, il montre un cerf marchant dans la forêt dense, vous semblez percevoir une vague silhouette désinée derrière, bien que ce ne soit peut-être que votre imagination. À votre gauche, la cuisine, à coté de la porte de la chambre de votre mère."
+Desc1_12_backInside="Vous rentrez dedans, mais la maison est termble, ça a l'air très dangereux, vous sortez de la maison pour l'instant. Vous revenez dehors, les cris coninuent."
+AskP1_backOutside=[Default_ask, "Courir vers les cris", "Courir dans la direction opposée aux cris"]
+
+QOut1_room=["Desc1_2_downstairs", "Desc1_4_books"]
+
+Desc1_8_outside="Vous franchissez la grande porte en chêne sombre et arrivez sur un champ de gravier, entouré de hautes herbes vertes. Vous respirez profondément en sentant l'air parfumé d'herbe rafraîchir vos poumons. Autour de vous vous voyez un certain nombre de maisons similaires, avec des petites maisons et des feuilles brillantes dans leur pelouse. Vous voyez un enfant jouer dans la maison d'en face, comment est-ce qu'il a l'air innocent..."
+Desc1_9_boom="Un flash soudain de lumière aveugle vos yeux, suivi d'une détonation assourdissante et d'une onde de choc, qui vous font perdre l'équilibre et tomber. Vous entendez des cris à travers le bourdonnement de vos oreilles venant du sud-est : c'est où est le magasin de mana !"
+AskP1_boom=[Default_ask, "Retourner chez vous", "Courir vers le magasin de mana", "Courir vers la direction opposée au son"]
+
+QOut1_downstairs=["Desc1_8_outside", "Desc1_10_backUpstairs", "Desc1_11_lookAround"]
+
+Desc1_13_runScreams="Vous courez vers les cris, et semblez distinguer une silhouette à la fin de la rue."
+OSay1_14_momScream=["Inconnu",'"AAAAAAAAAAAAHHH, QUELQU\'UN, N\'IMPORTE QUI AIDEZ-MOI S\'IL VOUS PLAIT, JE VOUS EN SUPPLIE!"']
+Desc1_15_isThatSooo="C'est la voix de votre mère! Pendant que vous vous en rapprochez, vous la distinguez plus clairement, il n'y a plus de doute, c'est bien elle."
+PSay1_16_mom="MAMAAN !"
+OSay1_17_dontCome=["Maman",'"NON !!! {0}, ne t\'approche pas de moi!".format(Player.name)']
+Desc1_18_momDead="Une autre personne apparait derrière elle, attendez, non, ce n'est pas une personne... C'EST UN TROLL ! Il lève son poing géant, et écrase votre mère agonisante sous celui-ci, sans même utiliser son épée."
+OSay1_19_trollSpeak=["Troll", "'HEHEHEHA, SPRLOUF HOUMAIN ! OH! HOUMAIN!!'"]
+Desc1_20_chased="Le troll vous voit, et comence à marcher vers vous."
+AskP1_chased=[Default_ask, "Se sauver", "Affronter le Troll"]
+EndG1_troll="Le troll vous écrase avec son poing."
+Desc1_21_runTroll="Vous courez dans la direction opposée aussi rapidement que vous le pouvez. La vision d'une mare de sang avec ce que vous pourriez assumer être des restes humains vous frappe aux yeux en dépit de leur boulier de larmes. Dans une tentative de perdre la bête qui s'approche lentement derrière vous, vous tournez à gauche. Vous vouyez un vieillard, immobile, mais sans aucune blessure."
+
+Desc1_22_runOpposite="Vous courez dans une direction, tournant à chaque fois que vous entendez les cris augmenter, à gauche, et à gauche encore. Vous voyez un vieillard, immobile, déphasé, et sans aucune blessure."
+
+OSay1_23_kamisaGo=["Kamisa", '"Mon enfant. Entre dans cette maison, choisis une arme pour ton aventure. Et bats-toi."']
+Desc1_24_Insinct="Pour une raison étrange, vous ne l'interrogez pas une seconde. Vous entrez dans la maison et voyez trois armes: Un grimoire ancien, une épée en bois, un bouclier en bois. Vous avez un préssentiment qu'une fois que vous choisissez une arme, vous ne pourrez jamais revenir en arrière."
+AskP1_weaponChoice=["Lequel préfèrez-vous ?", "L'Épée", "Le Bouclier", "Le Tome Magique"]
+
+SetV1_25_sword=["Weapon", "Wooden_Sword"]
+SetV1_26_shield=["Weapon", "Wooden_Shield"]
+SetV1_27_book=["Weapon", "Old_Grimoire"]
+SetV1_34_slash=["Attack_Slots", "[Slash]*3"]
+SetV1_35_flame=["Attack_Slots", "[Flame]*3"]
+
+OSay1_28_kamisaFight=["Kamisa", '"Les Trolls peuvent être, dans des temps normaux, les êtres les plus dangeureux dans ce royaume, mais tu peux le battre. Je t\'aiderai, héros. Il ne peut pas de tuer, son arme ne te prendra qu\'une fraction de ta vie, terrifiant si tu en possèdes beaucoup, mais pour un faible comme toi, c\'est une bénédiction. Je vais l\'affaiblir, tu l\'achèveras."']
+Desc1_29_preFight="Un flash lumineux de lumière vous force à fermer les yeux, et une fois que vous les ouvrez, le troll semble proche de la mort."
+Cmbt1_30_troll=['Vous vous précipitez vers le troll, et engagez le combat',10,['Troll1']]
+Desc1_31_hide="Le troll est mort. Pas d'homme agé. Homme agé mort ? Vous devez vous cacher. Cacher dans la maison. Vous êtes cachés dans la maison."
+Desc1_32_wait="Vous attendez pendant des minutes, des heures ou même peut-être des jours. Vous vous cachez jusqu'a ne plus rien entendre, rien que le silence. Alors vous ouvrez la porte, et vous voyez les cendres, rien ne reste d'une aussi grande ville que des cendres. Sauf la seule maison dans laquelle vous viviez, complètement intacte. Vous sortez, regardez autour de vous, rien d'autre que l'odeur de sang brulé. Vous regardez derrière vous, la maison est partie. Vous tombez à genoux."
+PSay1_33_alone="Pourquoi..? Pourquoi je suis vivant ? POURQUOI JE SUIS VIVANT ‽‽ Je les hais. Je vais tous les tuer. Chacun d'entre eux. JE VAIS OBLITÉRER TOUS LES TROLLS SUR CETTE PLANÈTE !"
+EndG1_demoOver="FIN, pour l'instant. Le reste arrive bientôt avec un soudain racisme contre les trolls"
+
+QOut1_weaponChoice=["SetV1_25_sword", "SetV1_26_shield", "SetV1_27_book"]
+QOut1_chased=["Desc1_21_runTroll", "EndG1_troll"]
+QOut1_boom=["Desc1_12_backInside", "Desc1_13_runScreams", "Desc1_22_runOpposite"]
+QOut1_backOutside=["Desc1_13_runScreams", "Desc1_22_runOpposite"]
