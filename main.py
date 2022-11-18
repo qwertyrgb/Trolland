1e61ca4fb75f77150af861520f18571eb172d071
qwertyrgb <68125220+qwertyrgb@users.noreply.github.com>
2022-11-18 17:39:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,25 @@
 from random import randint
 from continuity import *
 
-print('----------------------------------------\n                SETUP')
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
+try:
+  from pygame import mixer
+  playmusic=True
+  mixer.init()
+  mixer.Channel(0).play(mixer.Sound('lol.mp3'))
+except ModuleNotFoundError:
+  print("Vous jouerez sans tah la musique, c'est vla dommage")
+  print('Installez le module pygame pour avoir la musique')
+  playmusic=False
+
+decobar='----------------------------------------'
+print(decobar)
+print('                SETUP')
 try: #import basique de sauvegarde
   from saves import *
-  print('Loading save..')
+  print('Loading save...')
 except ModuleNotFoundError: #si non sauvegardé, pas de fichier de sauvegarde
 	print('No save detected')
 
@@ -22,7 +37,7 @@ else:
   from dialogue_en import * #Anglais par défaut
 
 Username=input('Enter username: ')
-print('----------------------------------------')
+print(decobar)
 
 def center(q,what):
 	'''centre un texte horizontalement'''
@@ -55,23 +70,26 @@ def say(msg,what,who=''):#dire des messages avec une décoration
 		q.append(msg[prev:i])
 		prev=i+1
 		i+=30
-	q.append(msg[prev:i])
+	q.append(msg[prev:i])#centre un texte verticalement
 	j=0
 	while j<len(q):
 		what(who,center(q[j:j+10],what))#Faire plusieurs bulles en cas de texte trop long
 		j+=10
 
-def exit():
+def exit_game():
   '''Sort du jeu et sauvegarde dans saves.py''' 
   save=open('saves.py','w')
+  print('currentAction='+str(currentAction),file=save)
   if Weapon!='':
     print('Weapon="'+list(globals())[list(globals().values()).index(Weapon)]+'"',file=save)
-    print('Attack_Slots="'+list(globals())[list(globals().values()).index(Attack_Slots)]+'"',file=save)
-  print('currentAction='+str(currentAction),file=save)
-  print("Weapon=''",file=save)
-  print("Attack_Slots=''",file=save)
+    print('Attack_Slots='+str([i.name for i in eval(list(globals())[list(globals().values()).index(Attack_Slots)])]),file=save)
+  else:
+    print("Weapon=''",file=save)
+    print("Attack_Slots=''",file=save)
+  quit()
   
 def load():
+  '''Charge le jeu comme PCB'''
   from saves import Weapon,Attack_Slots,currentAction
   global Weapon,Attack_Slots,currentAction
   if Weapon!='':
@@ -85,50 +103,59 @@ def load():
 
 def stats():
   '''Affiche les statistiques'''
-  print('----------------------------------------')
+  print(decobar)
   if Weapon!='':
     Weapon.info()#voir fonction info
-    print('Attack_Slots:',[i.name for i in Attack_Slots])
-    print('----------------------------------------')
+    print(Generic_attackslots,'\t'.join([i.name for i in Attack_Slots]))
+    print(decobar)
   else:
-    print('Weapon: None')
-    print('Attack_Slots: None')
+    print(Generic_weapon, Generic_none)
+    print(Generic_attackslots, Generic_none)
     
 def combat(desc,xp,enemies):
   '''Le nom n'explique pas son utilité?'''
+  ennemy_bar='{0.name} | LVL: {0.LVL} | HP: {0.HP}/{0.HPMAX} \n----------------------------------\n|{1}| \n----------------------------------'
   print(desc)
   enemies=[eval(i) for i in enemies]#chaque ennemi converti de string en objet
   def turn():#combat au tour par tour, surement pas en temps réel
     for Enemy in enemies:#pour tous les ennemis:
-      if Enemy.HP>0:# lignes longues,on n'aime pas la PEP 8
-        print('{0.name} | LVL: {0.LVL} | HP: {0.HP}/{0.HPMAX} \n----------------------------------\n|{1}| \n----------------------------------\n'.format(Enemy,'■'*int(32*(Enemy.HP/Enemy.HPMAX))+'□'*int(32-32*(Enemy.HP/Enemy.HPMAX))))
+      if Enemy.HP>0:
+        print(ennemy_bar.format(Enemy,'■'*int(32*(Enemy.HP/Enemy.HPMAX))+'□'*int(32-32*(Enemy.HP/Enemy.HPMAX))))#lignes longues,on n'aime pas la PEP 8
         Player.HP-=Enemy.attack.fight(Player)#L'ennemi attaque
         if Player.HP<=0:
-          print('You died! :(')#si votre HP<=0, vous êtes mort
-          return
-      else:print(Enemy.name,'dead')#Si son HP<=0, l'ennemi est mort
-      print(Enemy.name,'attacks',Player.name,'with',Enemy.attack.name)#shirAko attacks PEP 8 with very long lines
-    print('{0.name} | LVL: {0.LVL} | HP: {0.HP}/{0.HPMAX}\n----------------------------------\n|{1}|\n----------------------------------\n| {2[0].name} | {2[1].name} | {2[2].name} |\n----------------------------------\n'.format(Player,'■'*int(32*(Player.HP/Player.HPMAX))+'□'*int(32-32*(Player.HP/Player.HPMAX)),Attack_Slots))
+          print(Generic_youdied)#si votre HP<=0, vous êtes mort
+          print(Generic_gameover)
+          exit_game()
+      else:print(Enemy.name,Generic_dead)#Si son HP<=0, l'ennemi est mort
+      print(Enemy.name,Generic_attacks,Player.name,Generic_with,Enemy.attack.name,'!')#shirAko attacks PEP 8 with very long lines!
+    print(ennemy_bar.format(Player,'■'*int(32*(Player.HP/Player.HPMAX))+'□'*int(32-32*(Player.HP/Player.HPMAX))))
+    print('{0[0].name} | {0[1].name} | {0[2].name} |\n----------------------------------\n'.format(Attack_Slots))#slots d'attaques
     if len(enemies)>1:#S'il y a plus d'un ennemi, faire choisir au joueur la cible
-      rep=input('Target: ')
-      if rep=='stats':#le joueur à tout de même le droit de voire ses statistiques
+      rep=input(Generic_target)
+      while rep=='stats':#le joueur à tout de même le droit de voire ses statistiques
         stats()
-        rep=input('Target: ')
+        rep=input(Generic_target)
       if rep=='exit':#Le joueur peut quitter le jeu en le sauvegardant
-        exit()
-        return
+        exit_game()
+      while not rep.isdigit():
+        rep=input(Generic_target)
       target=enemies[int(rep)-1]#choisit la cible
     else:
       target=enemies[0]#si il y a un seul ennemi, le joueur n'aura pas le choix
-    rep=input('Attack: '+'\t'.join(['. '.join([str(i),Attack_Slots[i-1].name]) for i in range(1,len(Attack_Slots)+1)])+'\n')
-    if rep=='stats':
+    pickattack='Attack: '+'\t'.join(['. '.join([str(i),Attack_Slots[i-1].name]) for i in range(1,len(Attack_Slots)+1)])+'\n'
+    rep=input(pickattack)
+    while rep=='stats':
       stats()
+      rep=input(pickattack)
     if rep=='exit':
-      exit()
-      return
-    target.HP-=Attack_Slots[int(rep)].fight(target)#Finalement, vous attaquez
+      exit_game()
+    while not rep.isdigit() or int(rep)<1 or int(rep)>3:
+      rep=input(pickattack)
+    target.HP-=Attack_Slots[int(rep)-1].fight(target)#Finalement, vous attaquez
+    print(Player.name,Generic_attacks,Enemy.name,Generic_with,Attack_Slots[int(rep)].name,'!')
     if all(i.HP<=0 for i in enemies):
-      print('You killed all the ennemies and won the fight!')#GG
+      if playmusic:mixer.Channel(0).play(mixer.Sound('hall.mp3'))
+      print(Generic_win)#GG
       return
     return 1
   while turn():
@@ -156,7 +183,7 @@ class Entities: # définit toutes les entités du jeu (joueur, enemis..)
 		self.attack=attack
 
 Player=Entities(Username,0,10,20,0,0,1)#Vous
-Troll1=Entities('Injured troll',0,6,100,0,0,1,Smash)#Premier ennemi
+Troll1=Entities(Troll1_name,0,6,100,0,0,'??',Smash)#Premier ennemi
 Troll1.HP=6#affaiblissement
 
 # Définition des fonctions qui affectent les stats avec les armes
@@ -207,12 +234,12 @@ class Weapons: # définit toutes les armes du jeu
     '''Donne les informations sur l'arme sous forme de GUI'''
     print(self.name+', Tier:',self.tier+'\n'+', '.join(tuple(': '.join((i,writebonus(self.bonus[i]))) for i in self.bonus))+'\n'+self.description)
 
-Wooden_Shield=Weapons(Common_Tier,Wooden_Shield_Name,Wooden_Shield_Description) #pas de bonus dans power-system.txt
+Wooden_Shield=Weapons(Common_Tier,Wooden_Shield_Name,Wooden_Shield_Description) #pas de bonus
 Wooden_Sword=Weapons(Common_Tier,Wooden_Sword_Name,Wooden_Sword_Description)
 Old_Grimoire=Weapons(Common_Tier,Old_Grimoire_Name,Old_Grimoire_Description)
 Priest_Shield=Weapons(Rare_Tier,Priest_Shield_Name,Priest_Shield_Description,{'DEF':10})
 Blacksmith_Sword=Weapons(Rare_Tier,Blacksmith_Sword_Name,Blacksmith_Sword_Description,{'STA':5,'ATQ':10})
-Magical_Tome=Weapons(Rare_Tier,Magical_Tome_Name,Magical_Tome_Description,{'MP':5, 'ATQ':10}) #voir power-system.txt + dialogue_en.py
+Magical_Tome=Weapons(Rare_Tier,Magical_Tome_Name,Magical_Tome_Description,{'MP':5, 'ATQ':10})
 # 'FUN': fonction qui s'applique à chaque attaque, dans Faith_Shield la défense à une chance sur 10 de doubler (ligne 21)
 Faith_Shield=Weapons(Hero_Tier,Faith_Shield_Name,Faith_Shield_Description,{'DEF':25,'FUN':[faith_shield_fun,Faith_shield_fun_desc]})
 Michael_Sword=Weapons(Hero_Tier,Michael_Sword_Name,Michael_Sword_Description,{'STA':20,'ATQ':40,'FUN':[michael_sword_fun,Michael_sword_fun_desc]})
@@ -235,31 +262,34 @@ except ModuleNotFoundError: #si non sauvegardé, pas de fichier de sauvegarde
   Attack_Slots=['']*3
   currentAction=0
 
-def anal(q):#anal yse les types d'action de dialogue_en
+Generic_gameover="   _____                                                              _ \n  / ____|                                                            | |\n | |  __    __ _   _ __ ___     ___      ___   __   __   ___   _ __  | |\n | | |_ |  / _` | | '_ ` _ \\   / _ \\    / _ \\  \\ \\ / /  / _ \\ | '__| | |\n | |__| | | (_| | | | | | | | |  __/   | (_) |  \\ V /  |  __/ | |    |_|\n  \\_____|  \\__,_| |_| |_| |_|  \\___|    \\___/    \\_/    \\___| |_|    (_)"
+
+def analyse(q):
+    '''Analyse les types d'action de dialogue.py'''
     global currentAction
     if q[:4]=='Desc':
         say(eval(q),desc)
         currentAction=int(q.split('_')[1])#voir continuity.py
     elif q[:4]=='AskP':
         print()
-        print(eval(q)[0],end='\t')
-        rep=input('\t'.join(['. '.join([str(i),eval(q)[i]]) for i in range(1,len(eval(q)))])+'\n').lower()
+        print(eval(q)[0],end='\t')#la question
+        rep=input('\t'.join(['. '.join([str(i),eval(q)[i]]) for i in range(1,len(eval(q)))])+'\n').lower()#met des nombres avec les options
         #easter(rep)
-        if rep=='stats':
+        while rep=='stats':
           print('Your stats:')
           stats()
-        if rep=='exit':
-          exit()
-          return
-        if rep=='load':
+          return 1
+        if rep in ['exit','quit','quitter','sortir','salir']:
+          exit_game()
+        if rep in ['load','charger','cargar']:
           try:
-            print('Loading last checkpoint...')
+            print(Generic_loading)
             load()
           except ModuleNotFoundError:
-            print('Savefile not found')
+            print(Generic_nosave)
         else:
           try:
-            return anal(eval('QOut'+q[4:])[int(rep)-1])
+            return analyse(eval('QOut'+q[4:])[int(rep)-1])
           except:
             print(Not_Accepted)
     elif q[:4]=='PSay':
@@ -269,6 +299,7 @@ def anal(q):#anal yse les types d'action de dialogue_en
       currentAction=int(q.split('_')[1])
       say(eval(eval(q)[1]),osay,eval(q)[0])
     elif q[:4]=='Cmbt':
+      if playmusic:mixer.Channel(0).play(mixer.Sound('raid.mp3'))
       combat(*eval(q))
       currentAction=int(q.split('_')[1])
     elif q[:4]=='SetV':
@@ -276,9 +307,9 @@ def anal(q):#anal yse les types d'action de dialogue_en
       exec(eval(q)[0]+'='+eval(q)[1],globals())
     elif q[:4]=='EndG':
       print(eval(q))
-      print("   _____                                                              _ \n  / ____|                                                            | |\n | |  __    __ _   _ __ ___     ___      ___   __   __   ___   _ __  | |\n | | |_ |  / _` | | '_ ` _ \\   / _ \\    / _ \\  \\ \\ / /  / _ \\ | '__| | |\n | |__| | | (_| | | | | | | | |  __/   | (_) |  \\ V /  |  __/ | |    |_|\n  \\_____|  \\__,_| |_| |_| |_|  \\___|    \\___/    \\_/    \\___| |_|    (_)")
+      print(Generic_gameover)
       return
     return 1
 
-while anal(Next[currentAction]):
+while analyse(Next[currentAction]):
   input()#attendre l'input de l'utilisateur pour continuer
